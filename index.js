3fc7c2791b4285e88c4ff5ef1e4db0eead266fbd
Chris Hultin <chris.hultin@gmail.com>
2021-10-17 13:08:30 -0600

    Updating Main Icon

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const IMGS = {
 	ore: "https://cdn.discordapp.com/attachments/762209257738207252/762209362332090378/ore.png",
 	station: "https://cdn.discordapp.com/attachments/762209257738207252/762209374071947284/station.png",
 	other: "https://cdn.discordapp.com/attachments/762209257738207252/762209367386488852/random-done.png",
-	ico: "https://cdn.discordapp.com/attachments/762209257738207252/762219304694972447/eh5Artboard_1.png",
+	ico: "https://cdn.discordapp.com/attachments/892150703341072497/899373165384466542/Icon.png",
 };
 
 app.get("/favicon.ico", (req, res) => {
